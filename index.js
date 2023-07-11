10523b898b800b60cb1e7380e1658d39eca794bf
Declan Brown <135834907+DeclanKBrown@users.noreply.github.com>
2023-07-11 20:03:36 +1000

    Completed New GUI

--- index.js
+++ index.js
@@ -5,21 +5,24 @@ let dims = document.getElementById('dims');
 let dims2 = document.getElementById('dims2');
 
 //Function to created grid
-function createGrid(lxw) {
-    localStorage.setItem('lxw', lxw);
+function createGrid(l, h) {
+    localStorage.setItem('l', l);
+    localStorage.setItem('h', h);
+
     //reset grid
     resetGrid();
     //columns
-    for (let i = 0; i < lxw; ++i){
+    for (let i = 0; i < l; ++i){
         //rows
-        for (let j = 0; j < lxw; ++j) {
+        for (let j = 0; j < h; ++j) {
             //create  grid sections
             const div = document.createElement('div');
             //add class
             div.classList.add('grid');
+
             //set grid section heaight to fit
-            let height = 900/lxw;
-            let width = 900/lxw;
+            let height = 619/ h;
+            let width = 1100/ l;
             div.style.cssText = `height: ${height}px; width: ${width}px;`;
             //add to DOM
             sketchPad.appendChild(div);
@@ -32,26 +35,33 @@ function createGrid(lxw) {
 
 //Set on page load
 window.addEventListener("load", () => {
-    let lxw = 16;
-    dims.value = lxw;
-    dims2.value = lxw;
-    createGrid(lxw);
+    let l = 16;
+    let h = 9;
+    dims.value = l;
+    dims2.value = h;
+    createGrid(l, h);
   });
 
 //Event listener to get user input 
 dims.addEventListener('change', (e) => {
-    lxw = parseInt(e.target.value);
+    l = parseInt(e.target.value);
     
     //Check if val is over 64
-    if (lxw > 64) {
+    if (l > 100) {
         dims.classList.add('error');
     } else {
         //remove error class
         dims.classList.remove('error');
         //Push value on change into disable input 2
-        dims2.value = lxw;
+        h = ((l * 9) / 16);
+        if (isNaN(h)) {
+            dims2.value = "";
+        } else {
+            console.log(h);
+            dims2.value = h;
+        }
         //Call create grid funcition
-        createGrid(lxw);
+        createGrid(l, h);
     }
 });
 
@@ -63,12 +73,13 @@ function addEvent(div) {
 }
 
 //Reset button 
-let resetBtn = document.getElementById('reset');
+let resetBtn = document.getElementById('left');
 resetBtn.addEventListener('click', resetButton);
 
 function resetButton() {
-    lxw = localStorage.getItem('lxw')
-    createGrid(lxw);
+    l = localStorage.getItem('l')
+    h = localStorage.getItem('h')
+    createGrid(l, h);
 }
 
 //function to rest grid
